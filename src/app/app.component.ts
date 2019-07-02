91030c3c65d9d1d1f1f79434ad0864603eac3486
Jeko <jeko@Jeko.local>
2019-07-02 18:27:37 +0200

    Adding dropdown logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,45 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  isMobileview = false;
+    isMobileview = false;
 
-  public ngOnInit(): void {
-    this.manageDimensions();
-  }
+    public dropdownNavbarOffsetLeft: number;
+    public showDropdown = false;
+    public dropdownType: number;
 
-  @HostListener("window:resize", ["$event"])
-  manageDimensions() {
-    (window.innerWidth <= 991) ? this.isMobileview = true : this.isMobileview = false;
-  }
+    public ngOnInit(): void {
+        this.manageDimensions();
+    }
+
+    public navigateTo(whereTo: string, isDropdown: boolean, event: any, dropdownType: number): void {
+        this.showDropdown = false;
+        // ^ Close the other in case
+
+        if (isDropdown) {
+            this.dropdownNavbarOffsetLeft = event.currentTarget.offsetLeft;
+            this.dropdownType = dropdownType;
+            this.showDropdown = true;
+        } else {
+            // TODO with ngIf & dynamic components
+            this.resetDropdownNavbarStuff();
+        }
+
+        console.log(this.showDropdown);
+    }
+
+    private resetDropdownNavbarStuff(): void {
+        this.dropdownNavbarOffsetLeft = 0;
+        this.dropdownType = -1;
+        this.showDropdown = false;
+    }
+
+    @HostListener("window:resize", ["$event"])
+    manageDimensions() {
+        (window.innerWidth <= 991) ? this.isMobileview = true : this.isMobileview = false;
+    }
 }
