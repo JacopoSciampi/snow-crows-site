dc9947223efeb816c5dc687211759583034b01b4
Jacopo <jacopo.sciampi@arcoda.it>
2019-07-02 16:56:28 +0200

    Final update b4 real work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -10,7 +9,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
