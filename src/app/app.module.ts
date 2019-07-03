c91846f9c76e05b339efcbeeb375ebb6dae90aea
Jeko <jeko@Jeko.local>
2019-07-03 18:04:35 +0200

    adding footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { HomeInfoComponent } from './shared/components/_home/_home/info/home.info.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeInfoComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FooterComponent,
+        NavbarComponent,
+        HomeInfoComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
